c80ca5ad5429af18b6775f42cdd5107efc85be4e
Monyechi <monyechi@gmail.com>
2026-02-15 19:38:42 +0800

    > git add -A -- .gradle/8.10.2/checksums/checksums.lock .gradle/8.10.2/checksums/md5-checksums.bin .gradle/8.10.2/checksums/sha1-checksums.bin .gradle/8.10.2/fileHashes/fileHashes.lock .gradle/buildOutputCleanup/buildOutputCleanup.lock app/build.gradle.kts build.gradle.kts .gradle/8.10.2/executionHistory/executionHistory.bin .gradle/8.10.2/executionHistory/executionHistory.lock .gradle/8.10.2/fileHashes/fileHashes.bin .gradle/buildOutputCleanup/outputFiles.bin .gradle/file-system.probe app/build/generated/source/buildConfig/debug/com/monyechi/aistorysculptor/BuildConfig.java error: read error while indexing .gradle/8.10.2/checksums/checksums.lock: Permission denied error: .gradle/8.10.2/checksums/checksums.lock: failed to insert into database error: unable to index file '.gradle/8.10.2/checksums/checksums.lock' fatal: updating files failed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.kapt")
